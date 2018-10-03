c453fccd7c0cb518902972069748195ee5e02855
Iryna Mamutkina <ira.mamutkina@gmail.com>
2018-10-02 23:13:51 -0700

    Fixture is common for all test in file conftest.py

---
+++ test/test_del_group.py
@@ -0,0 +1,7 @@
+
+def test_delete_first_group(app):
+    app.navigation.open_home_page()
+    app.sessoin.login(login="admin", password="secret")
+    app.group.open_groups_page()
+    app.group.delete_first_group()
+    app.sessoin.logout()

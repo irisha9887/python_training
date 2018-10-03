18b893bc96941c49590bfa2d2e76b693054049c9
Iryna Mamutkina <ira.mamutkina@gmail.com>
2018-10-03 00:54:52 -0700

    Test_modify_contact was added

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -6,4 +6,4 @@ def test_modify_group(app):
     app.sessoin.login(login="admin", password="secret")
     app.group.open_groups_page()
     app.group.modify_first_group(Group(name="Changed group name", header="Changed header", footer="Changed footer"))
-
+    app.sessoin.logout()

2f8f1c3c77cf5a7152efabcbadf5e0d1790c9d50
Iryna Mamutkina <ira.mamutkina@gmail.com>
2018-10-02 23:53:20 -0700

    Test_delete_contact was added

---
+++ test/test_del_contat.py
@@ -0,0 +1,8 @@
+
+def test_delete_first_contact(app):
+    app.navigation.open_home_page()
+    app.sessoin.login(login="admin", password="secret")
+    app.navigation.open_home_page()
+    app.contact.delete_first_contact()
+    app.sessoin.logout()
+

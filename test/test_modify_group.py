1eab742f8b0fef19a8e75cef4db07b34d691d460
Iryna Mamutkina <ira.mamutkina@gmail.com>
2018-10-03 00:17:57 -0700

    Test_modify_group was added

---
+++ test/test_modify_group.py
@@ -0,0 +1,9 @@
+from model.group import Group
+
+
+def test_modify_group(app):
+    app.navigation.open_home_page()
+    app.sessoin.login(login="admin", password="secret")
+    app.group.open_groups_page()
+    app.group.modify_first_group(Group(name="Changed group name", header="Changed header", footer="Changed footer"))
+

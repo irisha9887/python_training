9dac1a3440e286cf61ff8ff099da388b52d5c5f5
Iryna Mamutkina <ira.mamutkina@gmail.com>
2018-10-06 14:55:02 -0700

    Login method was added to fixture

--- conftest.py
+++ conftest.py
@@ -5,5 +5,7 @@ from fixture.application import Application
 @pytest.fixture(scope = "session")
 def app(request):
     fixture = Application()
+    fixture.navigation.open_home_page()
+    fixture.session.login(login="admin", password="secret")
     request.addfinalizer(fixture.destroy)
     return fixture

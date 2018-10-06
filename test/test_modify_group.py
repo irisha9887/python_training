226e5e8d14784aeb69f22972dd6c13bf547e9179
Iryna Mamutkina <ira.mamutkina@gmail.com>
2018-10-05 21:58:04 -0700

    Test_modify_group was added with new implementation

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,9 +1,14 @@
 from model.group import Group
 
 
-def test_modify_group(app):
+def test_modify_first_group_name(app):
     app.navigation.open_home_page()
     app.session.login(login="admin", password="secret")
-    app.group.open_groups_page()
-    app.group.modify_first_group(Group(name="Changed group name", header="Changed header", footer="Changed footer"))
+    app.group.modify_first_group(Group(name="New group name"))
+    app.session.logout()
+
+def test_modify_first_group_header(app):
+    app.navigation.open_home_page()
+    app.session.login(login="admin", password="secret")
+    app.group.modify_first_group(Group(header="New header"))
     app.session.logout()

5e10c426b415235bc97498b264495b6ce70a72d5
Iryna Mamutkina <ira.mamutkina@gmail.com>
2018-10-07 23:03:02 -0700

    Some methods were modified (task 10)

--- fixture/session.py
+++ fixture/session.py
@@ -18,7 +18,6 @@ class SessionHelper:
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("user")
 
 
     def is_logged_in(self):

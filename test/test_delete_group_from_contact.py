df5f10da5bb69f2353cf183adea07193cc499ec3
Iryna Mamutkina <ira.mamutkina@gmail.com>
2018-11-29 02:01:55 -0800

    Try to create test: delete_contact_from_group

--- test/test_delete_group_from_contact.py
+++ test/test_delete_group_from_contact.py
@@ -12,18 +12,18 @@ def test_delete_group_from_contact(app, orm):
     group_list = orm.get_group_list()
     # Get contact list from random selected group
     selected_group = random.choice(group_list)
-    if len(orm.get_contacts_in_group(selected_group)) == 0:
-        app.navigation.open_edit_page()
-        app.contact.create(
-            Contact(firstname="Ekaterina", middlename="E.Smithes", lastname="Smithes", nickname="Cat",
-                    photo="/Users/i.mamutkina/Desktop/photo.png", title="Manager",
-                    company="Facebook", address="13 Main str, 9 apr, SF", home="1122334455", mobile="33221166554",
-                    work="99887744556",
-                    fax="fax-745-126-789", email="cat_1@gmail.com", email2="cat_2@gmail.com",
-                    email3="cat_3@gmail.com", homepage="Changed homepage",
-                    bday="10", bmonth="April", byear="1998", aday="19", amonth="December", ayear="1999",
-                    address2="123 Main str, 40 apr, San Carlos",
-                    phone2="385263354", notes="New changed notes!"))
+    #if len(orm.get_contacts_in_group(selected_group)) == 0:
+        #app.navigation.open_edit_page()
+        #app.contact.create(
+            #Contact(firstname="Ekaterina", middlename="E.Smithes", lastname="Smithes", nickname="Cat",
+                    #photo="/Users/i.mamutkina/Desktop/photo.png", title="Manager",
+                    #company="Facebook", address="13 Main str, 9 apr, SF", home="1122334455", mobile="33221166554",
+                    #work="99887744556",
+                    #fax="fax-745-126-789", email="cat_1@gmail.com", email2="cat_2@gmail.com",
+                    #email3="cat_3@gmail.com", homepage="Changed homepage",
+                    #bday="10", bmonth="April", byear="1998", aday="19", amonth="December", ayear="1999",
+                    #address2="123 Main str, 40 apr, San Carlos",
+                    #phone2="385263354", notes="New changed notes!"))
     contact_list = orm.get_contacts_in_group(selected_group)
     if len(contact_list) == 0:
         app.navigation.open_home_page()
@@ -39,18 +39,17 @@ def test_delete_group_from_contact(app, orm):
                         bday="10", bmonth="April", byear="1998", aday="19", amonth="December", ayear="1999",
                         address2="123 Main str, 40 apr, San Carlos",
                         phone2="385263354", notes="New changed notes!"))
-        selected_contact = random.choice(orm.get_contact_list())
-        app.contact.select_contact_by_id(selected_contact.id)
-        app.contact.add_group_to_contact(selected_contact.id)
+    selected_contact = random.choice(orm.get_contact_list())
+    #app.contact.select_contact_by_id(selected_contact.id)
+    app.contact.add_group_to_contact(selected_contact.id)
     # Get count of contacts which are inside selected group
     count_contacts_in_group_before_deleting_contact = len(contact_list)
     # Choose one random contact from this list
     selected_contact = random.choice(contact_list)
     app.contact.open_group_page_with_contacts(selected_group.name)
-    #ind = mylist.index(mycontact)
     contact_index = contact_list.index(selected_contact)
     app.contact.delete_group_from_contact(selected_group.name, contact_index)
-    ontact_list = orm.get_contacts_in_group(selected_group)
+    contact_list = orm.get_contacts_in_group(selected_group)
     count_contacts_in_group_after_deleting_contact = len(contact_list)
     assert count_contacts_in_group_before_deleting_contact - 1 == count_contacts_in_group_after_deleting_contact
 
